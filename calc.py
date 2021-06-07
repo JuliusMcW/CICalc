2121d81ec3e5e53fbe380683beb11f67f2d23eb6
JuliusMcW <63387539+JuliusMcW@users.noreply.github.com>
2021-06-06 19:33:33 -0700

    four method calc

---
+++ calc.py
@@ -0,0 +1,21 @@
+
+
+
+
+#op1 = float(input('Enter Operand 1');
+#op2 = float(input('Enter Operand 2');
+
+def addition(op1,op2):
+    return op1 +op2
+
+
+def subtraction(op1,op2):
+    return op1-op2
+
+def multiplication(op1,op2):
+    return op1*op2
+
+def division(op1,op2):
+    return op1/op2
+
+
